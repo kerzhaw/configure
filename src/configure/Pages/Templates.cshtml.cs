dabf6e9cccf016044c40bcf30271128b4cc0dc96
Paul Kershaw <Paul.Kershaw@optimationgroup.com>
2018-12-15 20:20:14 +1300

    add templates page

---
+++ src/configure/Pages/Templates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace configure.Pages
+{
+    public class TemplatesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
